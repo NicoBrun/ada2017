89e49e84acf54986bc4cc126f87894d65e691366
sgPepper <quentin.bouvet@hotmail.fr>
2017-12-15 09:52:38 +0100

    small changes

---
+++ project/io_helpers.py
@@ -0,0 +1,27 @@
+####
+####    IO helpers for the ADA project
+####
+####
+
+
+import pandas as pd
+import gzip
+
+
+
+''' This function was provided on the amazon dataset's webpage
+    It loads a gzipped file directly into a dataframe
+'''
+def json_gz_to_dataframe(filepath):
+    def parse(path): 
+        g = gzip.open(path, 'rb') 
+        for l in g: 
+            yield eval(l) 
+    def getDF(path): 
+        i = 0 
+        df = {} 
+        for d in parse(path): 
+            df[i] = d 
+            i += 1 
+        return pd.DataFrame.from_dict(df, orient='index') 
+    return getDF(filepath)
